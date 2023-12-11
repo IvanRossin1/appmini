c6631ce7bfef6a3896755c8a1d1cb28a35430695
IvanRossin1 <ivannrossini@gmail.com>
2023-12-11 16:27:53 -0300

    rafce

---
+++ src/components/principal/categoria.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const categoria = () => {
+  return (
+    <div>categoria</div>
+  )
+}
+
+export default categoria
---
+++ src/components/principal/principal.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const principal = () => {
+  return (
+    <div>principal</div>
+  )
+}
+
+export default principal
